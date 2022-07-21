bbc9eca6863761f257cb87c6d7981d4072b579eb
Holly Guevara <hollylawly@gmail.com>
2022-07-21 13:02:51 -0700

    remove timestamps

--- database/migrations/2021_12_20_194637_create_stars_table.php
+++ database/migrations/2021_12_20_194637_create_stars_table.php
@@ -15,7 +15,6 @@ class CreateStarsTable extends Migration
     {
         Schema::create('stars', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
             $table->string('name');
             $table->string('constellation');
         });
